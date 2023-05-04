f3a1282fb70bd75abbf2d1be88e9e494ab910257
Vladislav <tokenvlad@gmail.com>
2023-05-04 11:55:20 +0200

    first commit in game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Main {
         System.out.println("Ты готова поиграть и наконец-то узнать, что тебя ждет в конце этой игры? (ДА/НЕТ)");
         String answer = sc.nextLine();
 
+        int correctAnswers = 0;
         if (answer.equalsIgnoreCase("ДА")) {
             String[] words = {"Hund", "Milch", "Cola", "Wasser", "Montag",
                     "Wochenende", "Juni", "Kilogramm", "Land", "Ort",
@@ -18,12 +20,37 @@ public class Main {
                     "das", "der", "das", "das", "der",
                     "das", "der", "die", "das", "der",
                     "der", "der", "die", "die", "das", "die"};
-            int countAnswers = 0;
+            correctAnswers = 0;
 
             for (int i = 0; i < words.length; i++) {
                 System.out.println("А ну, давайка введи правильный артикль для слова " + words[i] + " :");
                 String article = sc.nextLine();
+
+                // Задержка перед выводом ответа
+                System.out.println("Подожди, я спрашиваю у Фран Еноты ...");
+                try {
+                    TimeUnit.SECONDS.sleep(0
+                    );
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+                if (article.equalsIgnoreCase(articles[i])) {
+                    correctAnswers++;
+                    System.out.println("Richtig");
+
+                } else {
+                    System.out.println("Falsch! Правильный ответ будет :" + articles[i]);
+                }
             }
         }
+        System.out.println("Бубка, ты правильно ответила на " + correctAnswers + "из 20 вопросов!");
+        if (correctAnswers >= 16) {
+            System.out.println("Отлично, ты умница! Можешь заглянуть в ");
+        } else {
+            System.out.println("Нужно выполнить условия игры, ничего не поделать, попробуй еще!");
+        }
     }
+
 }
+
